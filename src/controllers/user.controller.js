81445bc97f59b14382b3a90f5cc8ff63573f680f
Chandra Shekhar <jmbd.group@gmail.com>
2024-04-07 09:13:44 +0530

    fix bub for refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,25 +7,22 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 
 
 
-const generateAccessAndRefreshTokens = async(userId) =>{ 
-    try{
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
         const user = await User.findById(userId)
         const accessToken = user.generateAccessToken()
         const refreshToken = user.generateRefreshToken()
-        
 
         user.refreshToken = refreshToken
-        await user.save({validateBeforeSave: fales})
+        await user.save({ validateBeforeSave: false })
 
         return {accessToken, refreshToken}
 
 
-    } catch (error){
-        throw new ApiError(500, "Something went wrong while refresh and access token")
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
     }
-
 }
-
 
 
 const registerUser = asyncHandler(async (req, res)=>{
@@ -177,7 +174,7 @@ const loginUser = asyncHandler(async (req, res) => {
     
 
     // access and refresh token generate 
-    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
 
 
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
